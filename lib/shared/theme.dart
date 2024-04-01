b38fff7faab4b25917ce0fc16757ecb2220e5d65
Kevin T <kevinkho14@gmail.com>
2024-04-01 12:23:04 +0700

    commit pertama

---
+++ lib/shared/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData theme(){
+  return ThemeData(
+    primaryColor: Colors.green[700],
+    primaryColorLight: Colors.lightGreen[50],
+    primaryColorDark: Colors.lightGreen[500],
+    scaffoldBackgroundColor:Colors.lightGreen[50],
+    
+  );
+}
